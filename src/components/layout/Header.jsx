697fa82203b9fa3ee74f67fb40a976346c2eaf77
Couldevlop <coulwao@gmail.com>
2025-01-04 14:54:58 +0100

    Mise à jour du composant Header en chnageant le menu 'nous soliciter' par 'Commander'

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -67,7 +67,7 @@ const Header = () => {
                 </Link>
                 <Link to="/commande">
                   <button className="px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-300">
-                    Nous Soliciter
+                    Commander
                   </button>
                 </Link>
               </nav>
